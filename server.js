a0c10bf946ae44aac6698c669d6f5ac407beb580
Simon <simonanewton@gmail.com>
2020-05-11 20:55:10 -0400

    1.5 updated get notes API

--- server.js
+++ server.js
@@ -19,18 +19,18 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+app.get("/api/notes", (req, res) => {
+    return res.json(notes);
+});
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-//-----------------------------------------------------------------------------
-
-app.get("/api/notes", (req, res) => {
-    res.json(notes);
-});
-
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
+    notes.push(newNote);
+    res.json(true);
 });
 
 app.delete("/api/notes/:id", (req, res) => {});
