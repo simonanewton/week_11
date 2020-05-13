7c4e8f6ea60bd0605bd544cee00ec7a620110367
Simon <simonanewton@gmail.com>
2020-05-12 22:04:17 -0400

    1.8 added readFileSync function for getting notes data

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const notes = require("./db/db.json");
 
 const app = express();
 const port = process.env.port || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 //-----------------------------------------------------------------------------
 
@@ -20,15 +20,27 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        return data;
+    })); 
+
     return res.json(notes);
 });
 
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
     
+    const notes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        return data;
+    })); 
+
+    // newNote.id = Math.floor(Math.random() * 1000);
+
     notes.push(newNote);
 
-    fs.writeFile(".db/db.json", notes, (err) => {
+    fs.writeFile("./db/db.json", notes, (err) => {
         if (err) throw err;
     });
 
@@ -38,11 +50,16 @@ app.post("/api/notes", (req, res) => {
 app.delete("/api/notes/:id", (req, res) => {
     const noteId = req.params.id;
 
-    const index = notes.indexOf(noteId);
+    const notes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        return data;
+    })); 
 
-    notes.splice(index, 1);
+    // const index = notes.indexOf(noteId);
 
-    fs.writeFile(".db/db.json", notes, (err) => {
+    notes = notes.splice(index, 1);
+
+    fs.writeFile("./db/db.json", notes, (err) => {
         if (err) throw err;
     });
 });
