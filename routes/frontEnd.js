5537b4342bc43bf1a5b355c1f197d93c7b0cfe53
Simon Newton <simonanewton@gmail.com>
2020-05-16 20:27:18 -0400

    2.2 removed routes folder

--- routes/frontEnd.js
+++
@@ -1,7 +0,0 @@
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
